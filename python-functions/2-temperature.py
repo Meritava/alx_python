d33b0bc1f5b89789080dc9a0f51e00f87900ab37
Meritava <georgemeritava@gmail.com>
2023-07-26 04:50:46 +0100

    m

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,25 @@
-user_input = float(input("give temprature "))
+# fahrenheit = float(input("give temprature "))
+# def convert_to_celsius(fahrenheit):
+#     converted_temp = (fahrenheit - 32) * (5 / 9)
+#     print("{}".format(converted_temp))
+
+# # print("Temperature in Celsius = {:.2f}".formatconvert_to_celsius(fahrenheit))
+# convert_to_celsius(fahrenheit)
+
+# Define function to convert fahrenheit to celsius
 def convert_to_celsius(fahrenheit):
-    converted_temp = (fahrenheit - 32) * (5 / 9)
-    return converted_temp
-convert_to_celsius(400)
-print(convert_to_celsius)
+
+    # Convert the Fahrenheit into Celsius
+    C = (5 / 9) * (fahrenheit - 32)
+
+    # Return the conversion value
+    return C
+
+# Take the Fahrenheit value from the user
+fahrenheit = float(input("Enter the temperature in Fahrenheit: "))
+
+# Print the Fahrenheit value
+print("Temperature in Fahrenheit = {:.2f}".format(fahrenheit))
+
+# Print the Celsius value
+print("Temperature in Celsius = {:.2f}".format(convert_to_celsius(fahrenheit)))
