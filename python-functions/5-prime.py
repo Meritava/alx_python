9e88d872e0b8abf7be65e59d5537ef39068295cc
Meritava <georgemeritava@gmail.com>
2023-07-27 23:55:32 +0100

    m

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,5 +1,13 @@
+user_input = int(input("enter value "))
+
 def is_prime(number):
-    if number % number != 0:
-        print("True")
-    else:
-        print("False")
+    if number > 1:
+        for i in range(2, number):
+            if (number % i) != 0:
+                print("True")
+                break 
+            else:
+                print("False")
+
+
+is_prime(user_input)
