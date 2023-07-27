937d8c0832765ca4448d2c3eca2ee7c44db54386
Meritava <georgemeritava@gmail.com>
2023-07-27 21:41:23 +0100

    m

--- python-functions/3-string.py
+++ python-functions/3-string.py
@@ -1,5 +1,5 @@
-user_input = str(input("enter a string"))
+user_input = str(input("enter a string "))
 def reverse_string(string):
-    return string[::-1]
+    print(string[::-1])
 
 reverse_string(user_input)
