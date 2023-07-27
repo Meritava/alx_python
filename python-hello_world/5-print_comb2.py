2399fc4be01d7ab8a6be0dd83975854e09961c30
Meritava <georgemeritava@gmail.com>
2023-07-27 22:42:53 +0100

    m

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,8 +1,3 @@
-# for i in range(100):
-#     print("{:02d}".format(i),end=", ")
-#     if i < 99:
-#         print(end="\n")
-
 for number in range(100):
     if number <= 98:
         print("{:02d}".format(number), end=", ")
