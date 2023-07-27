5def3ede8ab889292e41717160a9aa0b68614281
Meritava <georgemeritava@gmail.com>
2023-07-27 22:25:00 +0100

    m

--- python-functions/2-temperature.py
+++ python-functions/2-temperature.py
@@ -1,6 +1,3 @@
-user_input = float(input("enter your temperature "))
 def convert_to_celsius(fahrenheit):
     celsius = (fahrenheit - 32) * 5 / 9
     return celsius
-
-convert_to_celsius(user_input)
