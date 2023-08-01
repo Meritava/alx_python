bbcb7461d4a48c2e964c62e028822f8245245826
Meritava <georgemeritava@gmail.com>
2023-08-01 13:55:00 +0100

    M

--- python-functions/5-prime.py
+++ python-functions/5-prime.py
@@ -1,5 +1,6 @@
 def is_prime(number):
-    if number > 1:
+    if number <= 1:
+        print("False")
         for i in range(2, number):
             if number % i != 0:
                 print("True")
