5c7bbefca7a01ffc06f3d46df5e5103b7031eff9
Meritava <georgemeritava@gmail.com>
2023-08-07 15:55:20 +0100

    last digit example

--- python-hello_world/3-last_digit.py
+++ python-hello_world/3-last_digit.py
@@ -2,19 +2,14 @@
 import random
 number = random.randint(-10000, 10000)
 # YOUR CODE HERE
-
-
-
-
-
-
-# #!/usr/bin/python3
-# import random
-# number = random.randint(-10000, 10000)
-# # YOUR CODE HERE
-# if number > 5:
-#     print("Last digit of {} is {} and is greater than 5".format(number, number))
-# elif number == 0:
-#     print("Last digit of {} is {} and is zero".format(number, number))
-# else:
-#     print("Last digit of {} is {} and is less than 6 and not 0\n".format(number, number))
+last_digit = abs(number) % 10
+if number < 0:
+    last_digit_with_sign = -last_digit
+else:
+    last_digit_with_sign = last_digit
+if last_digit_with_sign > 5:
+    print("Last digit of {} is {} and is greater than 5".format(number, last_digit_with_sign))
+elif last_digit_with_sign == 0:
+    print("Last digit of {} is {} and is 0".format(number, last_digit_with_sign))
+elif last_digit_with_sign < 6:
+    print("Last digit of {} is {} and is less than 6 and not 0".format(number, last_digit_with_sign))
