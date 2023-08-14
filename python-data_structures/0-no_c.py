c63ed0517a6e04ad7d1e29ee415eb7e9290ee7d4
Meritava <georgemeritava@gmail.com>
2023-08-14 14:13:34 +0100

    n

--- python-data_structures/0-no_c.py
+++ python-data_structures/0-no_c.py
@@ -1,9 +1,13 @@
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char != 'c' and char != 'C':
-            new_string += char
+        if char == 'c' and char == 'C':
+            my_string.remove(char)
         else:
             continue
+        new_string += char
         return new_string
-        
+    
+
+
+print(no_c("Chicago"))
