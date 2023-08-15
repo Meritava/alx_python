01e3c35115c528bc08562ae99a783d548f8569c0
Meritava <georgemeritava@gmail.com>
2023-08-15 15:09:47 +0100

    list common elements

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,3 +1,10 @@
 def common_elements(set_1, set_2):
-    for i in set_1 and set_2:
-        return i
+    #create a new set to store the common elements
+    common_set = set()
+    #iterate through the elements in set_1
+    for elements in set_1:
+        #condition to check if elements in set_1 are in set_2
+        if elements in set_2:
+            #add the elements to the new set store
+            common_set.add(elements)
+        return common_set
