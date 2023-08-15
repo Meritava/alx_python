bcf74838c6a1f4cdb7f491603417939e35c009e5
Meritava <georgemeritava@gmail.com>
2023-08-15 15:13:44 +0100

    m

--- python-more_data_structures/1-common_elements.py
+++ python-more_data_structures/1-common_elements.py
@@ -1,10 +1,12 @@
 def common_elements(set_1, set_2):
-    #create a new set to store the common elements
+    # create a new set to store the common elements
     common_set = set()
+    
     #iterate through the elements in set_1
     for elements in set_1:
-        #condition to check if elements in set_1 are in set_2
+        # conditional statement to check if elements in set one are in set two
         if elements in set_2:
-            #add the elements to the new set store
+            # add the elements to the new set store
             common_set.add(elements)
-        return common_set
+            
+    return common_set
