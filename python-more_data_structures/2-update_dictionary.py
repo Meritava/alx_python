448f9cc71953f9faf3b8d25ac1098ffb5fb8688c
Meritava <georgemeritava@gmail.com>
2023-08-15 15:34:17 +0100

    m

---
+++ python-more_data_structures/2-update_dictionary.py
@@ -0,0 +1,8 @@
+def update_dictionary(a_dictionary, key, value):
+    key = ""
+    if a_dictionary == None:   #check if dictionary is empty
+        a_dictionary = {}  #create an empty dictionary
+        a_dictionary[key] = value   #update the dictionary with new provided values
+    return a_dictionary
+
+    
