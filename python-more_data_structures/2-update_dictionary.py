e4b93b5cc8a24d48012ad9ed30e1c4afdeab72a7
Meritava <georgemeritava@gmail.com>
2023-08-15 15:38:04 +0100

    n

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,5 +1,6 @@
 def update_dictionary(a_dictionary, key, value):
-    key = ""
+    if not isinstance(key, str):
+        raise TypeError("Key must be a string") #ensure the key provided is a string
     if a_dictionary == None:   #check if dictionary is empty
         a_dictionary = {}  #create an empty dictionary
         a_dictionary[key] = value   #update the dictionary with new provided values
