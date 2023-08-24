a0567ee7b23d471b0106b9cb243ab3c1d9d6548c
Meritava <georgemeritava@gmail.com>
2023-08-24 14:09:33 +0100

    m

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,23 @@
+"""
+Module: geometry_classes
+
+This module defines base and derived geometric classes for calculations and validations.
+
+Classes:
+- BaseGeometry: A base class for geometric calculations.
+
+Module Author:
+Merit
+
+Last Updated:
+24/08/2023
+
+"""
+class BaseGeometry:
+    pass
+
+bg = BaseGeometry()
+
+print(bg)
+print(dir(bg))
+print(dir(BaseGeometry))
