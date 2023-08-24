2d8853efc3dc5a0bc9e94d8e7055c5cf9cbaeaa2
Meritava <georgemeritava@gmail.com>
2023-08-24 13:30:47 +0100

    m

---
+++ python-inheritance/2-inherits_from.py
@@ -0,0 +1,15 @@
+"""
+This module contains functions for class and object checks.
+"""
+def inherits_from(obj, a_class):
+    """
+    Check if an object is an instance of a specified class.
+
+    Args:
+    obj: The object to be checked for class membership.
+    a_class (class): The class to check against.
+
+    Returns:
+    bool: True if the object is an instance of the specified class, False otherwise.
+    """
+    return issubclass(obj, a_class)
