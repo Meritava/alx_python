3e2599a4847ad2dfc02c38069e857c5161e2740d
Meritava <georgemeritava@gmail.com>
2023-08-24 13:13:27 +0100

    m

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -15,5 +15,5 @@ def is_same_class(obj, a_class):
     if isinstance(obj, a_class):
         print("{} is an instance of the class {}".format(obj, a_class))
     else:
-        print("False")
+        return False
         
