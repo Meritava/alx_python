9d892ab543b0f4d2359f822c0396c2c971ce5e7e
Meritava <georgemeritava@gmail.com>
2023-08-24 15:52:56 +0100

    m

--- python-inheritance/3-base_geometry.py
+++ python-inheritance/3-base_geometry.py
@@ -17,4 +17,7 @@ class BaseGeometry:
     """
     a base class for gometry calculation
     """
-    pass
+    def __dir__(self):
+        attributes = super().__dir__()
+        new_attribute_list = [item for item in attributes if item != "__init_subclass__"]
+        return new_attribute_list
