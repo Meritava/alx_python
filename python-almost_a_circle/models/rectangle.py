c6690cebaf8515ec049f24301f0e94afe566b22d
Meritava <georgemeritava@gmail.com>
2023-08-31 04:06:15 +0100

    m

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -1,46 +1,71 @@
-"""
-Module: rectangle.py
-This module defines the Rectangle class, which inherits from the Base class.
-"""
+# models/rectangle.py
 from models.base import Base
 
 class Rectangle(Base):
-     """
-    Represents a rectangle, a subclass of the Base class.
+    """
+    Represents a rectangle with width, height, position (x, y), and an ID.
+    Inherits from the Base class for ID management.
 
     Attributes:
         width (int): The width of the rectangle.
         height (int): The height of the rectangle.
         x (int): The x-coordinate of the rectangle's position.
         y (int): The y-coordinate of the rectangle's position.
-        id (int): The unique identifier of the rectangle.
+        id (int): The unique ID of the rectangle inherited from the Base class.
     """
-     def __init__(self, width, height, x=0, y=0, id=None):
-          """
-        Initializes a Rectangle object.
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        Initializes a Rectangle instance.
 
         Args:
             width (int): The width of the rectangle.
             height (int): The height of the rectangle.
-            x (int, optional): The x-coordinate of the rectangle's position.
-            y (int, optional): The y-coordinate of the rectangle's position.
-            id (int, optional): The unique identifier of the rectangle.
+            x (int, optional): The x-coordinate of the rectangle's position. Defaults to 0.
+            y (int, optional): The y-coordinate of the rectangle's position. Defaults to 0.
+            id (int, optional): The unique ID of the rectangle. If not provided, it's managed by the Base class.
         """
-          super().__init__(id)
-          self.__width = width
-          self.__height = height
-          self.__x = x
-          self.__y = y
+        super().__init__(id)
+        self.__width = width
+        self.__height = height
+        self.__x = x
+        self.__y = y
 
-          @property
-          def width(self):
-               """
-               getter for the width
-               """
-               return self.__width
-          @width.setter
-          def width(self, value):
-               """
-               setter for width
-               """
-               self.__width = value
+    @property
+    def width(self):
+        """Getter for the width attribute."""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter for the width attribute."""
+        self.__width = value
+
+    @property
+    def height(self):
+        """Getter for the height attribute."""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter for the height attribute."""
+        self.__height = value
+
+    @property
+    def x(self):
+        """Getter for the x attribute."""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """Setter for the x attribute."""
+        self.__x = value
+
+    @property
+    def y(self):
+        """Getter for the y attribute."""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """Setter for the y attribute."""
+        self.__y = value
