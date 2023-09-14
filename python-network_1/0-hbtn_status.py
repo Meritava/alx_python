d86a53a2e330fe6f075ef6fe140933813b6bb271
Meritava <georgemeritava@gmail.com>
2023-09-14 14:12:10 +0100

    m

---
+++ python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+""" 
+This modules uses the request moduleno to get
+and fetch requests from alu-intranet
+"""
+import requests
+# a variable that stores the url to be fetch
+req = requests.get('https://alu-intranet.hbtn.io/status')
+print("Body response:")
+print("\t- type:".format(type(req.text)))
+print("\t- content:".format(req.text))
